7ffa82bb837b5488c6b9899344f39095dfa8add5
victor <vgm@gmail.com>
2025-09-08 11:16:38 +0200

    lienGithub.io du siteUrl changé

--- publishconf.py
+++ publishconf.py
@@ -9,6 +9,6 @@ from pelicanconf import *
 
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://Portfolio-SIO-victorgms.github.io"
+SITEURL = " https://victorgms3.github.io/victorgmsPortfolio.github.io/"
 RELATIVE_URLS = False
 DELETE_OUTPUT_DIRECTORY = True
